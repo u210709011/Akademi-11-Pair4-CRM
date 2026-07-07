e69f71ffa1c0a429d592153055333b7a864a9b9f
Yaren Aybüke Uludoğan (ETIYA) <yaren.uludogan@etiya.com>
2026-07-07 22:48:11 +0300

    main page structure created

---
+++ front-end/src/app/features/customer/create-customer/create-customer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-customer',
+  imports: [],
+  templateUrl: './create-customer.component.html',
+  styleUrl: './create-customer.component.scss'
+})
+export class CreateCustomerComponent {
+
+}
---
+++ front-end/src/app/features/customer/search-customer/search-customer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-customer',
+  imports: [],
+  templateUrl: './search-customer.component.html',
+  styleUrl: './search-customer.component.scss'
+})
+export class SearchCustomerComponent {
+
+}
