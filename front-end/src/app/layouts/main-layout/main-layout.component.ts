32c073540506b580fcae3a249c6c46d384eea689
Yaren Aybüke Uludoğan (ETIYA) <yaren.uludogan@etiya.com>
2026-07-07 23:08:49 +0300

    main page structure created

---
+++ front-end/src/app/layouts/main-layout/main-layout.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
+import { SidebarComponent } from '../../shared/components/sidebar/sidebar.component';
+
+@Component({
+  selector: 'app-main-layout',
+  imports: [RouterOutlet, NavbarComponent, SidebarComponent],
+  templateUrl: './main-layout.component.html',
+  styleUrl: './main-layout.component.scss'
+})
+export class MainLayoutComponent {
+
+}
