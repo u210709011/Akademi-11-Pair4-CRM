cf0dd671e97b62fda0d84eece2cfc52fc123070e
Abdullah Mert Şahin <mert.sahin@etiya.com>
2026-07-15 19:29:45 +0300

    added v1 to lookup api

--- back-end/customer-service/src/main/java/com/etiya/crm/customerservice/clients/controllers/LookupClient.java
+++ back-end/customer-service/src/main/java/com/etiya/crm/customerservice/clients/controllers/LookupClient.java
@@ -10,16 +10,16 @@ import com.etiya.crm.customerservice.clients.responses.LookupTpValueResponse;
 @FeignClient(name = "lookup-service")
 public interface LookupClient {
 
-	@GetMapping("/api/gnl_tp/{ent_code_name}/{tp_id}")
+	@GetMapping("/api/v1/gnl_tp/{ent_code_name}/{tp_id}")
 	LookupTpValueResponse getTpById(@PathVariable("ent_code_name") String ent_code_name, @PathVariable("tp_id") Long tp_id);
 
-	@GetMapping("/api/gnl_tp/{ent_code_name}/code/{shrt_code}")
+	@GetMapping("/api/v1/gnl_tp/{ent_code_name}/code/{shrt_code}")
 	LookupTpValueResponse getTpByCode(@PathVariable("ent_code_name") String ent_code_name, @PathVariable("shrt_code") String shrt_code);
 
 
-	@GetMapping("/api/gnl_st/{ent_code_name}/{st_id}")
+	@GetMapping("/api/v1/gnl_st/{ent_code_name}/{st_id}")
 	LookupStValueResponse getStById(@PathVariable("ent_code_name") String ent_code_name, @PathVariable("st_id") Long st_id);
 
-	@GetMapping("/api/gnl_st/{ent_code_name}/code/{shrt_code}")
+	@GetMapping("/api/v1/gnl_st/{ent_code_name}/code/{shrt_code}")
 	LookupStValueResponse getStByCode(@PathVariable("ent_code_name") String ent_code_name, @PathVariable("shrt_code") String shrt_code);
 }
