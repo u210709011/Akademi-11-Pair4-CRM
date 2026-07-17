b9b9e02b860e51977c5d29f2a3e23634c7a1ac7b
Abdullah Mert Şahin <mert.sahin@etiya.com>
2026-07-17 14:50:15 +0300

    some changes(idk)

--- back-end/lookup-service/src/main/java/com/etiya/crm/lookupservice/LookupServiceApplication.java
+++ back-end/lookup-service/src/main/java/com/etiya/crm/lookupservice/LookupServiceApplication.java
@@ -2,9 +2,7 @@ package com.etiya.crm.lookupservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing(auditorAwareRef = "jwtAuditorAware")
 @SpringBootApplication
 public class LookupServiceApplication {
 
