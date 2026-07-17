98205bf802faf0f7535e6b0e53bff2fcff72454d
Abdullah Mert Şahin <mert.sahin@etiya.com>
2026-07-17 15:01:39 +0300

    added v1 to lookup

--- back-end/lookup-service/src/main/resources/db/migration/V2__alter_lookup_audit_columns.sql
+++
@@ -1,5 +0,0 @@
--- cuser/uuser: JWT'den gelen preferred_username (string) ile hizalamak icin
--- BIGINT yerine VARCHAR. Diger servislerdeki (party/customer/contact-info)
--- JWT tabanli auditing ile ayni tip.
-ALTER TABLE lookup ALTER COLUMN cuser TYPE VARCHAR(255) USING cuser::varchar;
-ALTER TABLE lookup ALTER COLUMN uuser TYPE VARCHAR(255) USING uuser::varchar;
