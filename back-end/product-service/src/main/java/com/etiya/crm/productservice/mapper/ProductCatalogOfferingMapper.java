15eb690c3edb761f61813015de3be9dd2ffea698
Berat Tekeş <berat.tekes@outlook.com>
2026-08-04 09:21:22 +0300

    product specSrviceSpec oluşturuldu

--- back-end/product-service/src/main/java/com/etiya/crm/productservice/mapper/ProductCatalogOfferingMapper.java
+++ back-end/product-service/src/main/java/com/etiya/crm/productservice/mapper/ProductCatalogOfferingMapper.java
@@ -7,11 +7,13 @@ import com.etiya.crm.productservice.business.dtos.responses.ProductCatalogOfferi
 import com.etiya.crm.productservice.business.dtos.responses.ProductCatalogOffering.GetProductCatalogOfferingResponse;
 import com.etiya.crm.productservice.business.dtos.responses.ProductCatalogOffering.UpdatedProductCatalogOfferingResponse;
 import com.etiya.crm.productservice.entities.concretes.ProductCatalogOffering;
+import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 import java.util.List;
 
+@Mapper(componentModel = "spring")
 public interface ProductCatalogOfferingMapper {
 
     // ---------- CREATE ----------
